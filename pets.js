77fdec669dd318f0e138f7d54271d59e5dbfec9e
Johan Brattemark <johan.brattemark@gmail.com>
2017-05-30 14:40:46 -0700

    Refactor to have a function for reading JSON (async)

--- pets.js
+++ pets.js
@@ -3,23 +3,31 @@ let fs = require('fs');
 
 let fileName = path.basename(process.argv[1]);
 let subCommand = process.argv[2];
-let subArg = process.argv[3];
 
 if (!['read', 'create', 'update', 'destroy'].includes(subCommand)) {
   console.error(`Usage: node ${fileName} [read | create | update | destroy]`);
   process.exit(-1);
 }
 
+
+function getDataBase(dbFilePath, callback){
+  fs.readFile(dbFilePath, (err, data) => {
+    if(err) throw err;
+    var obj = JSON.parse(data);
+    callback(obj);
+  });
+}
+
+
 if (subCommand === 'read') {
-  fs.readFile('./pets.json', (err, data) => {
-    if (err) throw err;
-    var pets = JSON.parse(data);
-    if (subArg) {
-      if (subArg < 0 || subArg >= pets.length) {
+  getDataBase('./pets.json', pets => {
+    let index = process.argv[3];
+    if (index) {
+      if (index < 0 || index >= pets.length) {
         console.error(`Usage: node ${fileName} read INDEX`);
         process.exit(-1);
       } else {
-        console.log(pets[subArg]);
+        console.log(pets[index]);
       }
     } else {
       console.log(pets);
