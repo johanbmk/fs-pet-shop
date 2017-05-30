ef7ef30e6a91f292a196ec3f6b3471ec4f2066d9
Johan Brattemark <johan.brattemark@gmail.com>
2017-05-30 12:44:31 -0700

    Implement task 2, first part

--- pets.js
+++ pets.js
@@ -1,7 +1,18 @@
 let path = require('path');
+let fs = require('fs');
 
-if (!['read', 'create', 'update', 'destroy'].includes(process.argv[2])) {
+let subCommand = process.argv[2];
+
+if (!['read', 'create', 'update', 'destroy'].includes(subCommand)) {
   let fileName = path.basename(__filename);
   console.error(`Usage: node ${fileName} [read | create | update | destroy]`);
   process.exit(-1);
+}
+
+if (subCommand === 'read') {
+  fs.readFile('./pets.json', (err, data) => {
+    if (err) throw err;
+    var pets = JSON.parse(data);
+    console.log(pets);
+  });
 }
