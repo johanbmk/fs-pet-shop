d7e538a1d56875849fa90fc5fd4fe2d848044678
Johan Brattemark <johan.brattemark@gmail.com>
2017-05-30 16:58:53 -0700

    Implement 'destroy'

--- pets.js
+++ pets.js
@@ -8,14 +8,14 @@ let subCommand = process.argv[2];
 
 if (!['read', 'create', 'update', 'destroy'].includes(subCommand)) {
   console.error(`Usage: node ${fileName} [read | create | update | destroy]`);
-  process.exit(-1);
+  process.exit(1);
 }
 
 
 function getDataBase(dbFilePath, callback){
   fs.readFile(dbFilePath, (err, data) => {
     if(err) throw err;
-    var obj = JSON.parse(data);
+    let obj = JSON.parse(data);
     callback(obj);
   });
 }
@@ -27,7 +27,7 @@ if (subCommand === 'read') {
     if (index) {
       if (index < 0 || index >= pets.length) {
         console.error(`Usage: node ${fileName} read INDEX`);
-        process.exit(-1);
+        process.exit(1);
       } else {
         console.log(pets[index]);
       }
@@ -37,13 +37,14 @@ if (subCommand === 'read') {
   })
 }
 
+
 if (subCommand === 'create') {
   let age = process.argv[3];
   let kind = process.argv[4];
   let name = process.argv[5];
   if (isNaN(age) || !kind || !name) {
     console.error(`Usage: node ${fileName} create AGE KIND NAME`);
-    process.exit(-1);
+    process.exit(1);
   }
   let newPet = {};
   newPet.age = Number(age);
@@ -69,13 +70,33 @@ if (subCommand === 'update') {
   let name = process.argv[6];
   if (isNaN(index) || isNaN(age) || !kind || !name) {
     console.error(`Usage: node ${fileName} update INDEX AGE KIND NAME`);
-    process.exit(-1);
+    process.exit(1);
   }
   getDataBase(DB_FILE, pets => {
-    var pet = pets[index];
+    let pet = pets[index];
     pet.age = age;
     pet.kind = kind;
     pet.name = name;
+    let json = JSON.stringify(pets);
+    fs.writeFile(DB_FILE, json, (err) => {
+      if (err) {
+        throw err;
+      }
+    })
+    console.log(pet);
+  })
+}
+
+
+if (subCommand === 'destroy') {
+  let index = Number(process.argv[3]);
+  if (isNaN(index)) {
+    console.error(`Usage: node ${fileName} destroy INDEX`);
+    process.exit(1);
+  }
+  getDataBase(DB_FILE, pets => {
+    let pet = pets[index];
+    pets.splice(index, 1);
     let json = JSON.stringify(pets);
     fs.writeFile(DB_FILE, json, (err) => {
       if (err) {
