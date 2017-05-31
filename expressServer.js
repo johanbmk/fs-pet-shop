648c2416b8bd38a9f67d2f37e0b2f4569c0ae322
Johan Brattemark <johan.brattemark@gmail.com>
2017-05-31 15:27:22 -0700

    Implement all requirements for 'Express HTTP server' assignment

--- expressServer.js
+++ expressServer.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+const DB_FILE = './pets.json';
 
 app.get('/pets', (req, res) => {
-  let val = [{id: 1, color: 'red'}];
-  res.json(val);
+  getDataBase(DB_FILE, pets => {
+    res.json(pets);
+  })
 });
 
-app.get('/pets/:id', (req, res) => {
-  res.send(`You requested ID ${req.params.id}.`);
+app.get('/pets/:index', (req, res) => {
+  getDataBase(DB_FILE, pets => {
+    let index = req.params.index;
+    if (index < 0 || index >= pets.length) {
+      res.status(404);
+      res.set('content-type','text/plain');
+      res.send('Not Found');
+    } else {
+      res.json(pets[index]);
+    }
+  })
 });
 
 app.listen(8000, () => {
   console.log('Now Listening on port 8000');
 });
+
+
+function getDataBase(dbFilePath, callback){
+  fs.readFile(dbFilePath, (err, data) => {
+    if(err) throw err;
+    let obj = JSON.parse(data);
+    callback(obj);
+  });
+}
